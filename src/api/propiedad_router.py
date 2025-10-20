7505f786a8e307516c4cc944a5dc74524433cd93
Backyard <juabuey@gmail.com>
2025-10-20 09:43:57 -0300

    Corrige imports para usar PropiedadOut en GET /propiedades

--- src/api/propiedad_router.py
+++ src/api/propiedad_router.py
@@ -7,11 +7,21 @@ Created on Mon Oct 20 09:28:51 2025
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 from infrastructure.db import get_db
 from domain.models import Propiedad
+from domain.schemas import PropiedadInput, PropiedadOut
 
 router = APIRouter()
 
-@router.get("/propiedades")
+@router.get("/propiedades", response_model=List[PropiedadOut])
 def get_propiedades(db: Session = Depends(get_db)):
     return db.query(Propiedad).all()
+
+@router.post("/propiedades")
+def crear_propiedad(propiedad: PropiedadInput, db: Session = Depends(get_db)):
+    nueva = Propiedad(**propiedad.dict())
+    db.add(nueva)
+    db.commit()
+    db.refresh(nueva)
+    return nueva
